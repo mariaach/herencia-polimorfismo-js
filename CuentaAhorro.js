b2943b11cb773b875de857b31c5e630962794dd9
mariaach <cardenashenaomaria@gmail.com>
2023-03-30 17:36:36 -0500

    Nuevo archivo CuentaAhorros.js

---
+++ CuentaAhorro.js
@@ -0,0 +1,4 @@
+export class CuentaAhorro {#
+    cliente;#
+    saldo;
+}
--- index.js
+++ index.js
@@ -1,30 +1,27 @@
 /*Importación de clases*/
-import {Cliente} from './Cliente.js'
-import {CuentaCorriente} from './CuentaCorriente.js';
+import { Cliente } from './Cliente.js'
+import { CuentaCorriente } from './CuentaCorriente.js';
+import { CuentaAhorro } from './CuentaAhorro.js';
 
-const cliente = new Cliente('Leonardo','13804050','123224');
-const cliente2 = new Cliente('María','16979808','8989');
+const cliente = new Cliente('Leonardo', '13804050', '123224');
+const cliente2 = new Cliente('María', '16979808', '8989');
 
 const cuentaDeLeonardo = new CuentaCorriente(cliente, '1', '001');
-const cuentaDeMaria = new CuentaCorriente(cliente2,'2','002');
+const cuentaDeMaria = new CuentaCorriente(cliente2, '2', '002');
 
-let saldo = cuentaDeLeonardo.verSaldo(); 
+let saldo = cuentaDeLeonardo.verSaldo();
 
 saldo = cuentaDeLeonardo.depositoEnCuenta(150);
-cuentaDeLeonardo.transferirParaCuenta(parametroValor,cuentaDeMaria);
+cuentaDeLeonardo.transferirParaCuenta(parametroValor, cuentaDeMaria);
 
 const saldoMaria = cuentaDeMaria.verSaldo();
 
-console.log('Cuenta de Maria',cuentaDeMaria);
+console.log('Cuenta de Maria', cuentaDeMaria);
 
-console.log('El Saldo actual (cuentaMaria) '+saldoMaria);
+console.log('El Saldo actual (cuentaMaria) ' + saldoMaria);
 
 const saldoLeonardo = cuentaDeLeonardo.verSaldo();
-console.log('El Saldo actual (cuentaLeonardo) '+saldoLeonardo);
+console.log('El Saldo actual (cuentaLeonardo) ' + saldoLeonardo);
 console.log(CuentaCorriente.cantidadCuentas);
 
 console.log(CuentaCorriente.cantidadCuentas);
-
-
-
-
