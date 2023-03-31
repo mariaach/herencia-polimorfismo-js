4a623955bc027db591f3ecef9600a4740c3cd3f7
mariaach <cardenashenaomaria@gmail.com>
2023-03-31 10:15:11 -0500

    archivo cuentaAhorro.js

--- index.js
+++ index.js
@@ -9,19 +9,7 @@ const cliente2 = new Cliente('María', '16979808', '8989');
 const cuentaDeLeonardo = new CuentaCorriente(cliente, '1', '001');
 const cuentaDeMaria = new CuentaCorriente(cliente2, '2', '002');
 
-let saldo = cuentaDeLeonardo.verSaldo();
+const CuentaAhorroLeonardo = new CuentaAhorro(cliente, '9985', '001', 0);
 
-saldo = cuentaDeLeonardo.depositoEnCuenta(150);
-cuentaDeLeonardo.transferirParaCuenta(parametroValor, cuentaDeMaria);
-
-const saldoMaria = cuentaDeMaria.verSaldo();
-
-console.log('Cuenta de Maria', cuentaDeMaria);
-
-console.log('El Saldo actual (cuentaMaria) ' + saldoMaria);
-
-const saldoLeonardo = cuentaDeLeonardo.verSaldo();
-console.log('El Saldo actual (cuentaLeonardo) ' + saldoLeonardo);
-console.log(CuentaCorriente.cantidadCuentas);
-
-console.log(CuentaCorriente.cantidadCuentas);
+console.log(cuentaDeLeonardo);
+console.log(CuentaAhorroLeonardo);
