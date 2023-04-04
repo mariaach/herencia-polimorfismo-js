e32050dcc4d47e2b360ce2f6f3d926121b6899d3
mariaach <cardenashenaomaria@gmail.com>
2023-04-03 20:36:57 -0500

    clases Abstractas

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 /*Importación de clases*/
-import { Cliente } from './Cliente.js'
-import { CuentaCorriente } from './CuentaCorriente.js';
-import { CuentaAhorro } from './CuentaAhorro.js';
+import { Cliente } from './Cliente.js';
+import { CuentaCorriente } from './Cuentas/CuentaCorriente.js';
+import { CuentaAhorro } from './Cuentas/CuentaAhorro.js';
+import { Cuenta } from './Cuentas/Cuenta.js';
+import { CuentaNomina } from './Cuentas/CuentaNomina.js';
+import { Gerente } from './Empleados/Gerente.js';
+import { Director } from './Empleados/Director.js';
+import { Empleado} from './Empleados/Empleado.js';
+
 
 const cliente = new Cliente('Leonardo', '13804050', '123224');
 const cliente2 = new Cliente('María', '16979808', '8989');
 
-const cuentaDeLeonardo = new CuentaCorriente(cliente, '1', '001');
-const cuentaDeMaria = new CuentaCorriente(cliente2, '2', '002');
+const empleado = new Empleado('Juan Perez','1234343',10000);
+const gerente = new Gerente('Pedro Rivas','232344',12000);
+const director = new Director('Elena Moreno','232323',15000);
 
-const CuentaAhorroLeonardo = new CuentaAhorro(cliente, '9985', '001', 0);
 
-console.log(cuentaDeLeonardo);
-console.log(CuentaAhorroLeonardo);
+console.log(empleado.verBonificacion());
+console.log(gerente.verBonificacion());
+console.log(director.verBonificacion());
